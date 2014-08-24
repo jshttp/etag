ecc9a35d68fa790962c1b3a9b0bcf3b332bc292f
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-08-24 19:37:52 -0400

    tests: add test for empty content

--- test/test.js
+++ test/test.js
@@ -11,11 +11,19 @@ describe('etag(entity)', function () {
     it('should work containing Unicode', function () {
       assert.equal(etag('论'), '"aW9HeLTk2Yt6lf7zJYElgw=="')
     })
+
+    it('should work for empty string', function () {
+      assert.equal(etag(''), '"1B2M2Y8AsgTpgAmY7PhCfg=="')
+    })
   })
 
   describe('when "entity" is a Buffer', function () {
     it('should generate an ETag', function () {
       assert.equal(etag(new Buffer([1, 2, 3])), '"Uonfc331cyb83SJZevsfrA=="')
+    })
+
+    it('should work for empty Buffer', function () {
+      assert.equal(etag(new Buffer(0)), '"1B2M2Y8AsgTpgAmY7PhCfg=="')
     })
   })
 })
